81b345b20175e6b742eea4b0ac1ed4e1e12144b5
tyshantprince <tyshantprince@gmail.com>
2019-12-10 14:45:40 -0700

    first commit

---
+++ src/boot/chat.js
@@ -0,0 +1,6 @@
+// import something here
+import Vue from 'vue';
+import chat from 'vue-beautiful-chat';
+
+Vue.use(chat);
+
---
+++ src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Law For Kids Chat Test</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
